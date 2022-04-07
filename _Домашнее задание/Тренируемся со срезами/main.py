b37a416a6ce1d785d6cea51330b23b4a35c5f43c
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 20:26:29 +0300

    Тренируемся со срезами

---
+++ _Домашнее задание/Тренируемся со срезами/main.py
@@ -0,0 +1,12 @@
+alphabet = 'abcdefg'
+text = alphabet[:]
+print('1', text[::])
+print('2', text[::-1])
+print('3', text[::2])
+print('4', text[1::2])
+print('5', text[:1])
+print('6', text[6:])
+print('7', text[3:4])
+print('8', text[4:])
+print('9', text[3:5])
+print('10', text[4:2:-1])
